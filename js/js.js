fab0957e0bb8e0570a9e2fafaf21268d271c1c67
Your Name <you@example.com>
2020-12-15 13:12:43 +0700

    Alkhafi M.S

---
+++ js/js.js
@@ -0,0 +1,14 @@
+function kirim () {
+var x = document.getElementById("tanggal").value;
+
+if (x == "1994-12-14")
+{
+    alert("Selamat Ulang Tahun Ansar Muhammad Syam");
+    window.location = ("page.html")
+}
+    else
+{
+    alert("Tanggal Yang Anda Masukan Salah")
+}
+
+}
---
+++ js/jshbd.js
@@ -0,0 +1,5 @@
+alert("SENTUH KUENYA 🎂 DAN SELAMAT ULANG TAHUN CALL 🎶🎉🎊");
+window.addEventListener('click', function() {
+    var audio = document.getElementById("HBD");
+    audio.play();
+});
